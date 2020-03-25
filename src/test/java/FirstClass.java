9416d37a0866dc5a0fb0a72386442ca156a39237
doringraf <35940476+doringraf@users.noreply.github.com>
2020-03-25 19:55:22 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,14 +1,12 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("brave new word");
-        System.out.println("someone");
+        System.out.println("someone");   // comment
         System.out.println("change one");
         System.out.println("change two");
         System.out.println("change three");
         // first change
         // another change
-
-                // another change
-// another change
+        // another change
     }
 }
