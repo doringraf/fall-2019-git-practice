109b8b0073812e6ba8a7c799750030478f47f415
doringraf <doringraf@gmail.com>
2020-04-01 21:11:55 -0400

    sadad

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -1,2 +1,7 @@
 public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("hi everyone");
+        System.out.println("hi everyone");
+        System.out.println("hi everyone");
+    }
 }
